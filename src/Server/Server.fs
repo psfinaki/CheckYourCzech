9bdf37aaea31903121f257b5fd13a783710ee1f0
psfinaki <petersemkin@gmail.com>
2018-02-23 23:46:45 +0100

    Added basic SAFE app (#1)

---
+++ src/Server/Server.fs
@@ -0,0 +1,36 @@
+﻿open System.IO
+open System.Net
+
+open Suave
+open Suave.Operators
+
+
+open Shared
+
+let clientPath = Path.Combine("..","Client") |> Path.GetFullPath 
+let port = 8085us
+
+let config =
+  { defaultConfig with 
+      homeFolder = Some clientPath
+      bindings = [ HttpBinding.create HTTP (IPAddress.Parse "0.0.0.0") port ] }
+
+let getInitCounter () : Async<Counter> = async { return 42 }
+
+let init : WebPart = 
+  Filters.path "/api/init" >=>
+  fun ctx ->
+    async {
+      let! counter = getInitCounter()
+      return! Successful.OK (string counter) ctx
+    }
+
+let webPart =
+  choose [
+    init
+    Filters.path "/" >=> Files.browseFileHome "index.html"
+    Files.browseHome
+    RequestErrors.NOT_FOUND "Not found!"
+  ]
+
+startWebServer config webPart
