0cd4de77580691d8dca0cf725f33bc54fa8e3733
Peter Semkin <psfinaki@users.noreply.github.com>
2018-03-19 00:47:54 +0100

    Extremely rough page design (#23)

--- src/Client/pages/Multiples.fs
+++ src/Client/pages/Multiples.fs
@@ -1,6 +1,33 @@
 module Client.Multiples
 
+open Fable.Helpers.React
+open Fable.Helpers.React.Props
 open Style
 
 let view() =
-    [ words 60 "Write multiple for the word" ]
+    [ 
+      words 60 "Write multiple for the word" 
+      form [] [
+        div [ClassName ("form-group")] [
+            label [] [
+                str "Panda"
+            ]
+
+            input [ 
+                HTMLAttr.Type "text"
+            ]
+
+            button [] [
+                str "OK"
+            ]
+
+            label [] [
+                str "Correct!"
+            ]
+
+            button [] [
+                str "Repeat"
+            ]
+        ]
+      ]
+    ]
