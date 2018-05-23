e29cd84285ccf53bbdb06c0ae3e6890d5f2f555e
Peter Semkin <psfinaki@users.noreply.github.com>
2018-05-24 01:17:50 +0200

    Refactoring: separated getting answer functionality (#88)

--- src/Server/WebServer.fs
+++ src/Server/WebServer.fs
@@ -4,10 +4,7 @@ module ServerCode.WebServer
 open Giraffe
 open Giraffe.TokenRouter
 open RequestErrors
-open FSharp.Data
 open ServerCode
-
-type Wiki = HtmlProvider<"https://cs.wiktionary.org/wiki/panda">
 
 let webApp root =
     let notfound = NOT_FOUND "Page not found"
@@ -20,9 +17,7 @@ let webApp root =
 
     let getAnswer word : HttpHandler =
         fun _ ctx -> task {
-            let url = "https://cs.wiktionary.org/wiki/" + word
-            let data = Wiki.Load url
-            let answer = data.Tables.``Skloňování[editovat]``.Rows.[0].plurál
+            let answer = AnswerProvider.getMultiple word
             return! ctx.WriteJsonAsync answer 
         }
 
