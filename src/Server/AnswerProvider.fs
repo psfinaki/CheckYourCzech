a1bbac966b1a6419c0b21935c6f31819b9217687
Peter Semkin <psfinaki@users.noreply.github.com>
2018-05-29 02:09:15 +0200

    Identifying nouns without a plural (#112)

--- src/Server/AnswerProvider.fs
+++ src/Server/AnswerProvider.fs
@@ -8,4 +8,7 @@ let getPlural word =
     let url = "https://cs.wiktionary.org/wiki/" + word
     let data = Wiki.Load url
     let answer = data.Tables.``Skloňování[editovat]``.Rows.[0].plurál
-    answer.Split "/" |> Array.map (fun s -> s.Trim())
+    
+    match answer with
+    | "—" -> [||]
+    | _   -> answer.Split "/" |> Array.map (fun s -> s.Trim()) 
