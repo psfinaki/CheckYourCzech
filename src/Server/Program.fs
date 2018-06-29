005c3bd91bda8fc7ee368e1a404bbdb17c8b944a
psfinaki <petersemkin@gmail.com>
2018-06-29 20:39:54 +0200

    Moved to Saturn web server (#142)

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -1,81 +1,37 @@
-﻿module Program
+module Program
 
 open System
 open System.IO
-open Microsoft.AspNetCore
-open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
-open Newtonsoft.Json
-open Giraffe
-open Giraffe.Serialization.Json
+open Giraffe.Serialization
+open Saturn
 
 let GetEnvVar var =
     match Environment.GetEnvironmentVariable(var) with
     | null -> None
     | value -> Some value
 
-let getPortsOrDefault defaultVal =
-    match Environment.GetEnvironmentVariable("SUAVE_FABLE_PORT") with
-    | null -> defaultVal
-    | value -> value |> uint16
+let publicPath = GetEnvVar "public_path" |> Option.defaultValue "../Client/public" |> Path.GetFullPath
+let port = 8085us
 
-let configureApp root (app : IApplicationBuilder) =
-    app.UseStaticFiles()
-       .UseGiraffe (WebServer.webApp root)
-
-let configureServices (services : IServiceCollection) =
-    // Add default Giraffe dependencies
-    services.AddGiraffe() |> ignore
-
-    // Configure JsonSerializer to use Fable.JsonConverter
-    let fableJsonSettings = JsonSerializerSettings()
+let configureSerialization (services:IServiceCollection) =
+    let fableJsonSettings = Newtonsoft.Json.JsonSerializerSettings()
     fableJsonSettings.Converters.Add(Fable.JsonConverter())
+    services.AddSingleton<IJsonSerializer>(NewtonsoftJsonSerializer fableJsonSettings)
 
-    services.AddSingleton<IJsonSerializer>(
-        NewtonsoftJsonSerializer(fableJsonSettings)) |> ignore
+let configureAzure (services:IServiceCollection) =
+    GetEnvVar "APPINSIGHTS_INSTRUMENTATIONKEY"
+    |> Option.map services.AddApplicationInsightsTelemetry
+    |> Option.defaultValue services
 
-let configureLogging (loggerBuilder : ILoggingBuilder) =
-    loggerBuilder.AddFilter(fun lvl -> lvl.Equals LogLevel.Error)
-                 .AddConsole()
-                 .AddDebug() |> ignore
+let app = application {
+    url ("http://0.0.0.0:" + port.ToString() + "/")
+    router WebServer.webApp
+    memory_cache
+    use_static publicPath
+    service_config configureSerialization
+    service_config configureAzure
+    use_gzip
+}
 
-[<EntryPoint>]
-let main args =
-    try
-        let args = Array.toList args
-        let clientPath =
-            match args with
-            | clientPath:: _  when Directory.Exists clientPath -> clientPath
-            | _ ->
-                // did we start from server folder?
-                let devPath = Path.Combine("..","Client")
-                if Directory.Exists devPath then devPath
-                else
-                    // maybe we are in root of project?
-                    let devPath = Path.Combine("src","Client")
-                    if Directory.Exists devPath then devPath
-                    else @"./client"
-            |> Path.GetFullPath
-
-        let port = getPortsOrDefault 8085us
-
-        WebHost
-            .CreateDefaultBuilder()
-            .UseWebRoot(clientPath)
-            .UseContentRoot(clientPath)
-            .ConfigureLogging(configureLogging)
-            .ConfigureServices(configureServices)
-            .Configure(Action<IApplicationBuilder> (configureApp clientPath))
-            .UseUrls("http://0.0.0.0:" + port.ToString() + "/")
-            .Build()
-            .Run()
-        0
-    with
-    | exn ->
-        let color = Console.ForegroundColor
-        Console.ForegroundColor <- System.ConsoleColor.Red
-        Console.WriteLine(exn.Message)
-        Console.ForegroundColor <- color
-        1
+run app
--- src/Server/WebServer.fs
+++ src/Server/WebServer.fs
@@ -1,28 +1,22 @@
 ﻿module WebServer
 
 open Giraffe
-open Giraffe.TokenRouter
-open RequestErrors
+open Saturn
 open Gender
 
-let webApp root =
-    let notfound = NOT_FOUND "Page not found"
+let getTask gender : HttpHandler =
+    fun _ ctx -> task { 
+        let word = WordProvider.getNoun (translateTo gender)
+        return! ctx.WriteJsonAsync word
+    }
 
-    let getTask gender : HttpHandler =
-        fun _ ctx -> task { 
-            let word = WordProvider.getNoun (translateTo gender)
-            return! ctx.WriteJsonAsync word
-        }
+let getAnswer word : HttpHandler =
+    fun _ ctx -> task {
+        let answer = AnswerProvider.getPlural word
+        return! ctx.WriteJsonAsync answer 
+    }
 
-    let getAnswer word : HttpHandler =
-        fun _ ctx -> task {
-            let answer = AnswerProvider.getPlural word
-            return! ctx.WriteJsonAsync answer 
-        }
-
-    router notfound [
-        GET [
-            routef "/api/task/%s" getTask
-            routef "/api/answer/%s" getAnswer
-        ]
-    ]
+let webApp = scope {
+    getf "/api/task/%s" getTask
+    getf "/api/answer/%s" getAnswer
+}
