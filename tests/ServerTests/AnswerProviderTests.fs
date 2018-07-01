7178c6c03f55d2ac974e605598eba0a4de5e941f
psfinaki <petersemkin@gmail.com>
2018-07-02 00:47:11 +0200

    Refactoring: simplified namespace structure in tests

--- tests/ServerTests/AnswerProviderTests.fs
+++ tests/ServerTests/AnswerProviderTests.fs
@@ -1,61 +1,59 @@
-﻿namespace ServerTests
+﻿module AnswerProviderTests
 
 open Xunit
 open AnswerProvider
 open Gender
 
-module AnswerProviderTests =
+let equals (x: string[]) (y: string[]) = Assert.Equal<string []>(x, y)
 
-    let equals (x: string[]) (y: string[]) = Assert.Equal<string []>(x, y)
+[<Fact>]
+let getsPlural() = 
+    "svetr"
+    |> getPlural
+    |> equals [|"svetry"|]
 
-    [<Fact>]
-    let getsPlural() = 
-        "svetr"
-        |> getPlural
-        |> equals [|"svetry"|]
+[<Fact>]
+let getsPluralSeveralOptions() = 
+    "medvěd"
+    |> getPlural
+    |> equals [|"medvědi"; "medvědové"|]
 
-    [<Fact>]
-    let getsPluralSeveralOptions() = 
-        "medvěd"
-        |> getPlural
-        |> equals [|"medvědi"; "medvědové"|]
+[<Fact>]
+let getsPluralSeveralOptionsNoSpaces() = 
+    "Edáček"
+    |> getPlural
+    |> equals [|"Edáčci"; "Edáčkové"|]
 
-    [<Fact>]
-    let getsPluralSeveralOptionsNoSpaces() = 
-        "Edáček"
-        |> getPlural
-        |> equals [|"Edáčci"; "Edáčkové"|]
+[<Fact>]
+let getsPluralNoOptions() = 
+    "Oxford"
+    |> getPlural
+    |> equals [||] 
 
-    [<Fact>]
-    let getsPluralNoOptions() = 
-        "Oxford"
-        |> getPlural
-        |> equals [||] 
+[<Fact>]
+let getsGenderMasculineAnimate() =
+    "tata"
+    |> getGender
+    |> (=) MasculineAnimate
+    |> Assert.True
 
-    [<Fact>]
-    let getsGenderMasculineAnimate() =
-        "tata"
-        |> getGender
-        |> (=) MasculineAnimate
-        |> Assert.True
+[<Fact>]
+let getsGenderMasculineInanimate() =
+    "hrad"
+    |> getGender
+    |> (=) MasculineInanimate
+    |> Assert.True
 
-    [<Fact>]
-    let getsGenderMasculineInanimate() =
-        "hrad"
-        |> getGender
-        |> (=) MasculineInanimate
-        |> Assert.True
+[<Fact>]
+let getsGenderFeminine() =
+    "panda"
+    |> getGender
+    |> (=) Feminine
+    |> Assert.True
 
-    [<Fact>]
-    let getsGenderFeminine() =
-        "panda"
-        |> getGender
-        |> (=) Feminine
-        |> Assert.True
-
-    [<Fact>]
-    let getsGenderNeuter() =
-        "okno"
-        |> getGender
-        |> (=) Neuter
-        |> Assert.True
+[<Fact>]
+let getsGenderNeuter() =
+    "okno"
+    |> getGender
+    |> (=) Neuter
+    |> Assert.True
--- tests/ServerTests/WordProviderTests.fs
+++ tests/ServerTests/WordProviderTests.fs
@@ -1,54 +1,52 @@
-﻿namespace ServerTests
+﻿module WordProviderTests
 
 open Xunit
 open WordProvider
 
-module WordProviderTests =
+[<Fact>]
+let detectsCzechNoun() = 
+    "hudba"
+    |> isProperNoun
+    |> Assert.True
 
-    [<Fact>]
-    let detectsCzechNoun() = 
-        "hudba"
-        |> isProperNoun
-        |> Assert.True
+[<Fact>]
+let detectsNoCzechPart() = 
+    "immigration"
+    |> isProperNoun
+    |> Assert.False
 
-    [<Fact>]
-    let detectsNoCzechPart() = 
-        "immigration"
-        |> isProperNoun
-        |> Assert.False
+[<Fact>]
+let detectsNoCzechNoun() = 
+    "koukat"
+    |> isProperNoun
+    |> Assert.False
 
-    [<Fact>]
-    let detectsNoCzechNoun() = 
-        "koukat"
-        |> isProperNoun
-        |> Assert.False
+[<Fact>]
+let detectsNoCzechDeclination() = 
+    "antilopu"
+    |> isProperNoun
+    |> Assert.False
 
-    [<Fact>]
-    let detectsNoCzechDeclination() = 
-        "antilopu"
-        |> isProperNoun
-        |> Assert.False
+[<Fact>]
+let detectsPlural() =
+    "panda"
+    |> hasPlural
+    |> Assert.True
 
-    [<Fact>]
-    let detectsPlural() =
-        "panda"
-        |> hasPlural
-        |> Assert.True
+[<Fact>]
+let detectsNoPlural1() = 
+    "Oxford"
+    |> hasPlural
+    |> Assert.False
 
-    [<Fact>]
-    let detectsNoPlural1() = 
-        "Oxford"
-        |> hasPlural
-        |> Assert.False
-
-    [<Fact>]
-    let detectsNoPlural2() = 
-        "Pegas"
-        |> hasPlural
-        |> Assert.False
+[<Fact>]
+let detectsNoPlural2() = 
+    "Pegas"
+    |> hasPlural
+    |> Assert.False
         
-    [<Fact>]
-    let detectsGender() =
-        "panda"
-        |> hasGender Gender.Feminine
-        |> Assert.True
+[<Fact>]
+let detectsGender() =
+    "panda"
+    |> hasGender Gender.Feminine
+    |> Assert.True
