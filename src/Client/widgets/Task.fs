7dd221b2eb5acc0015b0cc0b511bc97c12b9dc5f
psfinaki <petersemkin@gmail.com>
2019-01-11 23:46:45 +0100

    Disabling Next and Check buttons when task is not ready (#280)

--- src/Client/widgets/Task.fs
+++ src/Client/widgets/Task.fs
@@ -5,6 +5,7 @@ open Fable.Helpers.React
 open Fable.PowerPack
 open Fable.Import.React
 open Fable.Core.JsInterop
+open Markup
 
 type Task = { 
     Word : string 
@@ -58,7 +59,7 @@ let view model dispatch =
         | Some result -> 
             let imageSource = if result then "images/correct.png" else "images/incorrect.png"
             let altText = if result then "Correct" else "Incorrect"
-            Markup.icon imageSource 25 altText
+            icon imageSource 25 altText
         | None ->
             str ""
 
@@ -69,14 +70,14 @@ let view model dispatch =
         | None ->
             let imageSource = "images/loading.gif"
             let altText = "Loading..."
-            Markup.icon imageSource 25 altText
+            icon imageSource 25 altText
 
     let handleChangeAnswer (event: FormEvent) =
         dispatch (SetInput !!event.target?value)
            
     let handleKeyDown (event: KeyboardEvent) =
         match event.keyCode with
-        | Keyboard.Codes.enter ->
+        | Keyboard.Codes.enter when model.Word.IsSome ->
             match event.shiftKey with
             | false -> dispatch SubmitAnswer
             | true  -> dispatch UpdateTask
@@ -86,21 +87,23 @@ let view model dispatch =
     let handleUpdateClick _ = dispatch UpdateTask
     let handleCheckClick _ = dispatch SubmitAnswer
     
+    let clickability = if model.Word.IsSome then Clickable else Unclickable
+
     div []
         [
             div [ Styles.row ] 
                 [
-                    Markup.label Styles.greyLabel task
-                    Markup.input Styles.input model.Input handleChangeAnswer handleKeyDown
-                    Markup.label Styles.greyLabel result
+                    label Styles.greyLabel task
+                    input Styles.input model.Input handleChangeAnswer handleKeyDown
+                    label Styles.greyLabel result
                 ]
 
-            Markup.emptyLines 2
+            emptyLines 2
 
             div [ Styles.row ]
                 [
-                    Markup.button (Styles.button "White") handleUpdateClick "Next (⇧ + ⏎)"
-                    Markup.space()
-                    Markup.button (Styles.button "Lime") handleCheckClick "Check (⏎)"
+                    button (Styles.button "White") handleUpdateClick "Next (⇧ + ⏎)" clickability
+                    space()
+                    button (Styles.button "Lime") handleCheckClick "Check (⏎)" clickability
                 ]
         ]
