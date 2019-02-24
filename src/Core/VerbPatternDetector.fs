ab5e398ef1a347057bc9ffb36ca46be6c0d2f1db
psfinaki <petersemkin@gmail.com>
2019-02-24 14:52:18 +0100

    Refactoring: extracted pattern detection to a separate module, uniforming terminology

---
+++ src/Core/VerbPatternDetector.fs
@@ -0,0 +1,80 @@
+﻿module VerbPatternDetector
+
+open StringHelper
+open Letters
+open Stem
+
+let isPatternTisknout word = 
+    let getStem = removeLast 4
+    
+    let isPattern letter = 
+        letter |> isConsonant && 
+        letter |> isNotSyllabicConsonant
+    
+    word |> ends "nout" && 
+    word |> getStem |> endsIf isPattern
+
+let isPatternMinout word = 
+    let getStem = removeLast 4
+    
+    let isPattern letter =
+        letter |> isVowel ||
+        letter |> isSyllabicConsonant
+
+    word |> ends "nout" &&
+    word |> getStem |> endsIf isPattern
+
+let isPatternProsit word = 
+    word |> ends "it" &&
+    word |> removeLast 2 |> takeLast 2 |> (not << isConsonantGroup)
+
+let isPatternČistit word =
+    word |> ends "it" &&
+    word |> removeLast 2 |> takeLast 2 |> isConsonantGroup
+
+let invalidVerb verb verbClass = 
+    sprintf "The verb %s does not belong to the class %i." verb verbClass
+    |> invalidArg "verb"
+
+let getPatternClass1 = function
+    | verb when verb |> ends "ést" -> "nést"
+    | verb when verb |> ends "íst" -> "číst"
+    | verb when verb |> ends "éct" -> "péct"
+    | verb when verb |> ends "ít" -> "třít"
+    | verb when verb |> ends "át" -> "brát"
+    | verb when verb |> ends "at" -> "mazat"
+    | verb -> invalidVerb verb 1
+
+let getPatternClass2 = function
+    | verb when verb |> isPatternTisknout -> "tisknout"
+    | verb when verb |> isPatternMinout -> "minout"
+    | verb when verb |> ends "ít" -> "začít"
+    | verb -> invalidVerb verb 2
+
+let getPatternClass3 = function
+    | verb when verb |> ends "ovat" -> "kupovat"
+    | verb when verb |> ends "ýt" -> "krýt"
+    | verb -> invalidVerb verb 3
+
+let getPatternClass4 = function
+    | verb when verb |> isPatternProsit -> "prosit"
+    | verb when verb |> isPatternČistit -> "čistit"
+    | verb when verb |> ends "ět" -> "trpět"
+    | verb when verb |> ends "et" -> "sázet"
+    | verb -> invalidVerb verb 4
+
+let getPatternClass5 = function
+    | verb when verb |> ends "at" -> "dělat"
+    | verb -> invalidVerb verb 5
+
+let patternClassMap =
+    dict [ (1, getPatternClass1)
+           (2, getPatternClass2)
+           (3, getPatternClass3)
+           (4, getPatternClass4)
+           (5, getPatternClass5) ]
+
+let getPatternByClass verb verbClass = 
+    verb 
+    |> Verb.removeReflexive 
+    |> patternClassMap.[verbClass]
---
+++ tests/Core.Tests/VerbPatternDetectorTests.fs
@@ -0,0 +1,149 @@
+﻿module VerbPatternDetectorTests
+
+open System
+open Xunit
+open VerbPatternDetector
+
+let equals (expected: 'T) (actual: 'T) = Assert.Equal<'T>(expected, actual)
+
+[<Fact>]
+let ``Detects pattern tisknout``() =
+    "tisknout"
+    |> isPatternTisknout
+    |> Assert.True
+
+[<Theory>]
+[<InlineData "dělat">]
+[<InlineData "minout">]
+[<InlineData "shrnout">]
+let ``Detects pattern is not tisknout`` word =
+    word
+    |> isPatternTisknout
+    |> Assert.False
+
+[<Theory>]
+[<InlineData "minout">]
+[<InlineData "shrnout">]
+[<InlineData "přilnout">]
+let ``Detects pattern minout`` word =
+    word
+    |> isPatternMinout
+    |> Assert.True
+
+[<Theory>]
+[<InlineData "dělat">]
+[<InlineData "tisknout">]
+let ``Detects pattern is not minout`` word =
+    word
+    |> isPatternMinout
+    |> Assert.False
+
+[<Theory>]
+[<InlineData "hlásit">]
+[<InlineData "řídit">]
+let ``Detects pattern prosit`` word =
+    word
+    |> isPatternProsit
+    |> Assert.True
+
+[<Theory>]
+[<InlineData "dělat">]
+[<InlineData "čistit">]
+let ``Detects pattern is not prosit`` word =
+    word
+    |> isPatternProsit
+    |> Assert.False
+
+[<Theory>]
+[<InlineData "mastit">]
+[<InlineData "zvýraznit">]
+let ``Detects pattern čistit`` word =
+    word
+    |> isPatternČistit
+    |> Assert.True
+
+[<Theory>]
+[<InlineData "prosit">]
+[<InlineData "dělat">]
+let ``Detects pattern is not čistit`` word =
+    word
+    |> isPatternČistit
+    |> Assert.False
+
+[<Theory>]
+[<InlineData("vést", "nést")>]
+[<InlineData("příst", "číst")>]
+[<InlineData("téct", "péct")>]
+[<InlineData("dřít", "třít")>]
+[<InlineData("prát", "brát")>]
+[<InlineData("vázat", "mazat")>]
+let ``Gets pattern class 1`` verb pattern =
+    verb
+    |> getPatternClass1
+    |> equals pattern
+
+[<Fact>]
+let ``Throws for invalid verb class 1``() =
+    let action () = getPatternClass1 "kreslit" |> ignore
+    Assert.Throws<ArgumentException> action
+
+[<Theory>]
+[<InlineData("prasknout", "tisknout")>]
+[<InlineData("hynout", "minout")>]
+[<InlineData("načít", "začít")>]
+let ``Gets pattern class 2`` verb pattern =
+    verb
+    |> getPatternClass2
+    |> equals pattern
+
+[<Fact>]
+let ``Throws for invalid verb class 2``() =
+    let action () = getPatternClass2 "kreslit" |> ignore
+    Assert.Throws<ArgumentException> action
+
+[<Theory>]
+[<InlineData("pracovat", "kupovat")>]
+[<InlineData("výt", "krýt")>]
+let ``Gets pattern class 3`` verb pattern =
+    verb
+    |> getPatternClass3
+    |> equals pattern
+
+[<Fact>]
+let ``Throws for invalid verb class 3``() =
+    let action () = getPatternClass3 "kreslit" |> ignore
+    Assert.Throws<ArgumentException> action
+
+[<Theory>]
+[<InlineData("nosit", "prosit")>]
+[<InlineData("mastit", "čistit")>]
+[<InlineData("sedět", "trpět")>]
+[<InlineData("házet", "sázet")>]
+let ``Gets pattern class 4`` verb pattern =
+    verb
+    |> getPatternClass4
+    |> equals pattern
+
+[<Fact>]
+let ``Throws for invalid verb class 4``() =
+    let action () = getPatternClass4 "dělat" |> ignore
+    Assert.Throws<ArgumentException> action
+
+[<Fact>]
+let ``Gets pattern class 5``() =
+    "létat"
+    |> getPatternClass5
+    |> equals "dělat"
+
+[<Fact>]
+let ``Throws for invalid verb class 5``() =
+    let action () = getPatternClass5 "kreslit" |> ignore
+    Assert.Throws<ArgumentException> action
+
+[<Theory>]
+[<InlineData("ohlásit", 4, "prosit")>]
+[<InlineData("ohlásit se", 4, "prosit")>]
+let ``Gets pattern by class`` verb verbClass pattern =
+    getPatternByClass verb verbClass
+    |> equals pattern
+
