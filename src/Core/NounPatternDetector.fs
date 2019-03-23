b45b5e164c74a6b35b3366f6d5ac3ecd802021df
psfinaki <petersemkin@gmail.com>
2019-03-23 18:02:20 +0100

    Detecting noun pattern (#406)

---
+++ src/Core/NounPatternDetector.fs
@@ -0,0 +1,39 @@
+﻿module NounPatternDetector
+
+open Genders
+open StringHelper
+open Stem
+
+let getPatternMasculineAnimate = function
+    | (nominative, genitive) when genitive |> ends "a" -> Some "pan"
+    | (nominative, genitive) when nominative |> endsSoft -> Some "muž"
+    | (nominative, genitive) when nominative |> ends "a" -> Some "předseda"
+    | (nominative, genitive) when nominative |> ends "ce" -> Some "soudce"
+    | _ -> None
+
+let getPatternMasculineInanimate = function
+    | (nominative, genitive) when genitive |> ends "u" || genitive |> ends "a" -> Some "hrad"
+    | (nominative, genitive) when genitive |> ends "e" -> Some "stroj"
+    | (nominative, genitive) -> invalidArg "noun" ("odd noun: " + nominative)
+
+let getPatternFeminine = function
+    | (nominative, genitive) when genitive |> ends "y" -> Some "žena"
+    | (nominative, genitive) when nominative |> ends "e" -> Some "růže"
+    | (nominative, genitive) when genitive |> ends "e" || genitive |> ends "ě" -> Some "píseň"
+    | (nominative, genitive) when genitive |> ends "i" -> Some "kost"
+    | _ -> None
+
+let getPatternNeuter = function
+    | (nominative, genitive) when genitive |> ends "a" -> Some "město"
+    | (nominative, genitive) when genitive |> ends "ete" -> Some "kuře"
+    | (nominative, genitive) when genitive |> ends "e" -> Some "moře"
+    | (nominative, genitive) when genitive |> ends "í" -> Some "stavení"
+    | _ -> None
+
+let patternGenderMap =
+    dict [ (MasculineAnimate, getPatternMasculineAnimate)
+           (MasculineInanimate, getPatternMasculineInanimate)
+           (Feminine, getPatternFeminine)
+           (Neuter, getPatternNeuter) ]
+
+let getPatternByGender gender = patternGenderMap.[gender]
