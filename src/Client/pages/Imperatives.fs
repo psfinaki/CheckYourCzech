2bba79da1ccaac6bfb5b8d6e45e62760c3721e2f
Peter Semkin <psfinaki@users.noreply.github.com>
2019-04-11 08:41:10 +0200

    Refactoring: merged Pattern and NounPattern (#433) (#434)

--- src/Client/pages/Imperatives.fs
+++ src/Client/pages/Imperatives.fs
@@ -16,6 +16,15 @@ type Msg =
     | Pattern of Pattern.Msg
     | Task of Task.Msg
     
+let patterns =
+    dict [ (1, ["nést"; "číst"; "péct"; "třít"; "brát"; "mazat"])
+           (2, ["tisknout"; "minout"; "začít"])
+           (3, ["krýt"; "kupovat"])
+           (4, ["prosit"; "čistit"; "trpět"; "sázet"])
+           (5, ["dělat"]) ]
+
+let getPatterns ``class`` = patterns.[``class``]
+
 let getTask ``class`` pattern =
     let classQuery = ``class`` |> Option.map (sprintf "class=%i")
     let patternQuery = pattern |> Option.map (sprintf "pattern=%s")
@@ -46,7 +55,8 @@ let update msg model =
     match msg with
     | Class msg' ->
         let ``class`` = Class.update msg' model.Class
-        let pattern = Pattern.update (Pattern.SetClass ``class``.Class) model.Pattern
+        let patterns = ``class``.Class |> Option.map getPatterns
+        let pattern = Pattern.update (Pattern.SetPatterns patterns) model.Pattern
         { model with 
             Class = ``class`` 
             Pattern = pattern
@@ -55,7 +65,7 @@ let update msg model =
         let pattern = Pattern.update msg' model.Pattern
         { model with Pattern = pattern }, Cmd.none
     | Task msg' ->
-        let task, cmd = Task.update msg' model.Task (getTask model.Class.Class model.Pattern.Pattern)
+        let task, cmd = Task.update msg' model.Task (getTask model.Class.Class model.Pattern.SelectedPattern)
         { model with Task = task }, Cmd.map Task cmd
 
 let view model dispatch = 
