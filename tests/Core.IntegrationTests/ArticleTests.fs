22623d60a292a19544a75742dac4ec708a95b872
psfinaki <petersemkin@gmail.com>
2019-04-14 14:38:08 +0200

    Protecting from Web exceptions (#444)

--- tests/Core.IntegrationTests/ArticleTests.fs
+++ tests/Core.IntegrationTests/ArticleTests.fs
@@ -6,6 +6,13 @@ open Article
 let equals (expected: 'T) (actual: 'T) = Assert.Equal<'T>(expected, actual)
 let equalsMany (x: string list) (y: seq<string>) = Assert.Equal<string list>(x, Seq.toList y)
 
+[<Fact>]
+let ``Detects no table of contents``() =
+    "alik"
+    |> tryGetTableOfContents
+    |> Option.isNone
+    |> Assert.True
+
 [<Theory>]
 [<InlineData "panda">]
 [<InlineData "mnoho myslivců – zajícova smrt">]
--- tests/Core.IntegrationTests/ImperativeTests.fs
+++ tests/Core.IntegrationTests/ImperativeTests.fs
@@ -10,3 +10,33 @@ let ``Validates proper verb``() =
     "spát"
     |> isValid 
     |> Assert.True
+
+[<Fact>]
+let ``Invalidates improper verb - no Czech``() =
+    "good"
+    |> isValid
+    |> Assert.False
+
+[<Fact>]
+let ``Invalidates improper verb - no verb``() =
+    "nazdar"
+    |> isValid
+    |> Assert.False
+
+[<Fact>]
+let ``Invalidates improper verb - no conjugation``() =
+    "chlastati"
+    |> isValid
+    |> Assert.False
+
+[<Fact>]
+let ``Invalidates improper verb - no imperative``() =
+    "moct"
+    |> isValid
+    |> Assert.False
+
+[<Fact>]
+let ``Invalidates improper verb - archaic``() =
+    "péci"
+    |> isValid
+    |> Assert.False
