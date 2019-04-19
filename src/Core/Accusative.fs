f7fba619e0a546aefa96fc6a6ffde86964ce0f75
Peter Semkin <psfinaki@users.noreply.github.com>
2019-04-19 11:06:02 +0200

    Moved noun validation to the Noun module (#448)

--- src/Core/Accusative.fs
+++ src/Core/Accusative.fs
@@ -1,44 +1,27 @@
 ﻿module Accusative
 
-open Article
-open Genders
 open Microsoft.WindowsAzure.Storage.Table
-open Noun
-
-let getNominatives = getDeclension Case.Nominative Number.Singular
-let getAccusatives = getDeclension Case.Accusative Number.Singular
 
 let isValid word = 
-    let nounPart = ArticleParser.tryGetNoun word
+    word |> Word.isNoun &&
+    word |> Noun.hasDeclension &&
+    word |> Noun.hasGender &&
+    word |> Noun.hasSingleDeclensionForCase Noun.Case.Nominative Noun.Number.Singular
 
-    let hasDeclension =
-        nounPart
-        |> Option.bind (tryGetPart "skloňování")
-        |> Option.isSome
-
-    let hasGender = 
-        nounPart
-        |> Option.bind (tryGetInfo "rod")
-        |> Option.bind tryTranslateGender
-        |> Option.isSome
-
-    let hasOneNominative = 
-        getNominatives
-        >> Seq.hasOneElement
-
-    hasDeclension && 
-    hasGender && 
-    hasOneNominative word
+let getNominative = Storage.mapSafeString id
+let getAccusatives = Storage.mapSafeString (Noun.getDeclensionForCase Noun.Case.Accusative Noun.Number.Singular)
+let getGender = Storage.mapSafeObject (Noun.getGender >> box)
+let getPattern = Storage.mapSafeStringOption Noun.getPattern 
 
 type Accusative(word) =
     inherit TableEntity(word, word)
     
     new() = Accusative null
 
-    member val Nominative  = word |> Storage.mapSafeString id                 with get, set
-    member val Gender      = word |> Storage.mapSafeObject (getGender >> box) with get, set
-    member val Pattern     = word |> Storage.mapSafeStringOption getPattern   with get, set
-    member val Accusatives = word |> Storage.mapSafeString getAccusatives     with get, set
+    member val Nominative = getNominative word with get, set
+    member val Gender = getGender word with get, set
+    member val Pattern = getPattern word with get, set
+    member val Accusatives = getAccusatives word with get, set
 
 let record word =
     if 
