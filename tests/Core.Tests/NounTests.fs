f7f5be230692642064fdfd92fc03a6e683758f4d
Peter Semkin <psfinaki@users.noreply.github.com>
2019-05-12 00:26:47 +0200

    Filtering out nominalizations (#485)

---
+++ tests/Core.Tests/NounTests.fs
@@ -0,0 +1,22 @@
+﻿module NounTests
+
+open Xunit
+open Noun
+
+let equals (expected: 'T) (actual: 'T) = Assert.Equal<'T>(expected, actual)
+
+[<Theory>]
+[<InlineData "Hrabovský">]
+[<InlineData "Abrahámová">]
+[<InlineData "znalečné">]
+[<InlineData "hovězí">]
+let ``Detects nominalization`` noun =
+    noun
+    |> isNominalization
+    |> Assert.True
+
+[<Fact>]
+let ``Detects no nominalization``() =
+    "láska"
+    |> isNominalization
+    |> Assert.False
