741e3703b794dd6f64bc17c67978a04e43350d1f
Peter Semkin <psfinaki@users.noreply.github.com>
2019-06-01 16:48:37 +0200

    Minor: simplified development storage connection string

--- build.fsx
+++ build.fsx
@@ -52,7 +52,7 @@ let openBrowser url =
 
 Target.create "SetEnvironmentVariables" (fun _ ->
     Environment.setEnvironVar "ASPNETCORE_ENVIRONMENT" "local"
-    Environment.setEnvironVar "STORAGE_CONNECTIONSTRING" "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;"
+    Environment.setEnvironVar "STORAGE_CONNECTIONSTRING" "UseDevelopmentStorage=true"
 )
 
 Target.create "InstallClient" (fun _ ->
