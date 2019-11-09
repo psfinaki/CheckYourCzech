ae4eaf409eea87a1cc062de0af77a6122d1cb8f9
psfinaki <petersemkin@gmail.com>
2019-11-09 23:44:58 +0100

    Adjustments for local development

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -24,7 +24,7 @@ let configureAzure (services:IServiceCollection) =
     |> Option.defaultValue services
 
 let app = application {
-    url ("http://0.0.0.0:" + port.ToString() + "/")
+    url ("https://0.0.0.0:" + port.ToString() + "/")
     force_ssl
     use_router WebServer.webApp
     memory_cache
