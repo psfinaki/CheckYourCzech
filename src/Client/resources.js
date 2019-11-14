5ba83a4361b99c915eabdad5fcda16a29e0b16b9
psfinaki <petersemkin@gmail.com>
2019-11-14 18:59:19 +0100

    Optimized working with images

--- src/Client/resources.js
+++ src/Client/resources.js
@@ -1,8 +1,3 @@
-import './images/correct.png';
-import './images/incorrect.png';
-import './images/question_mark.png';
-import './images/loading.gif';
-
 import './scss/main.scss';
 
 import './favicon.ico';
