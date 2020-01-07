dd9e95a3f7af7f3649f27be1147480f805762987
Peter Semkin <psfinaki@users.noreply.github.com>
2020-01-07 09:48:26 +0100

    Added the Rytmus pattern to masculine inanimates (#533)

--- src/Core/Nouns/MasculineInanimateNounPatternDetector.fs
+++ src/Core/Nouns/MasculineInanimateNounPatternDetector.fs
@@ -4,17 +4,41 @@ open NounArticle
 open StringHelper
 open GrammarCategories
 
-let isPatternHrad = 
-    getDeclension Case.Genitive Number.Singular
-    >> Seq.exists (endsOneOf ["u"; "a"])
+let canBeLoanword = endsOneOf ["us"; "es"; "os"]
+
+let isPatternHrad = function
+    | noun when noun |> canBeLoanword -> 
+        let singulars = noun |> getDeclension Case.Nominative Number.Singular
+        let plurals = noun |> getDeclension Case.Nominative Number.Plural
+        
+        let isPluralPatternHrad (singular, plural) = 
+            singular |> append "y" = plural
+
+        Seq.allPairs singulars plurals |> Seq.exists isPluralPatternHrad
+    | noun ->
+        noun
+        |> getDeclension Case.Genitive Number.Singular
+        |> Seq.exists (endsOneOf ["u"; "a"])
 
 let isPatternStroj =
     getDeclension Case.Nominative Number.Plural
     >> Seq.exists (endsOneOf ["e"; "ě"])
 
+let isPatternRytmus noun =
+    noun |> canBeLoanword && 
+
+    let singulars = noun |> getDeclension Case.Nominative Number.Singular
+    let plurals = noun |> getDeclension Case.Nominative Number.Plural
+
+    let isPluralPatternRytmus (singular, plural) = 
+        singular |> removeLast 2 |> append "y" = plural
+
+    Seq.allPairs singulars plurals |> Seq.exists isPluralPatternRytmus
+
 let patternDetectors = [
     (isPatternHrad, "hrad")
     (isPatternStroj, "stroj")
+    (isPatternRytmus, "rytmus")
 ]
 
 let isPattern word patternDetector = fst patternDetector word
