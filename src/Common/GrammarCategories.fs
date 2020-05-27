c29f99b0a5614a37a6ec9b4dc0e7cfe3d59f0760
Peter Semkin <petersemkin@gmail.com>
2020-05-27 09:46:04 +0200

    Changed case enum to case union

--- src/Common/GrammarCategories.fs
+++ src/Common/GrammarCategories.fs
@@ -1,13 +1,13 @@
 ﻿module GrammarCategories
 
 type Case = 
-    | Nominative = 0
-    | Genitive = 1
-    | Dative = 2
-    | Accusative = 3
-    | Vocative = 4
-    | Locative = 5
-    | Instrumental = 6
+    | Nominative
+    | Genitive
+    | Dative
+    | Accusative
+    | Vocative
+    | Locative
+    | Instrumental
 
 type Number =
     | Singular
--- src/WikiParsing/Articles/NounArticle.fs
+++ src/WikiParsing/Articles/NounArticle.fs
@@ -12,6 +12,15 @@ type EditableArticleOneDeclension = HtmlProvider<"https://cs.wiktionary.org/wiki
 type EditableArticleTwoDeclensions = HtmlProvider<"https://cs.wiktionary.org/wiki/čtvrt">
 type LockedArticle = HtmlProvider<"https://cs.wiktionary.org/wiki/debil">
 
+let caseColumns = 
+    dict [ Case.Nominative, 0
+           Case.Genitive, 1
+           Case.Dative, 2
+           Case.Accusative, 3
+           Case.Vocative, 4
+           Case.Locative, 5
+           Case.Instrumental, 6 ]
+
 let getNumberOfDeclensions (NounArticle article) =
     article
     |> matches [
@@ -20,37 +29,39 @@ let getNumberOfDeclensions (NounArticle article) =
     ]
     |> Seq.length
 
-let getEditable case number article =
+let getEditable (case: Case) number article =
     let numberOfDeclensions = getNumberOfDeclensions article
     let (NounArticle {Title = word; Text = text}) = article
 
+    let caseColumn = caseColumns.[case]
     match numberOfDeclensions with
     | 1 ->
         let data = EditableArticleOneDeclension.Parse text
         match number with
         | Singular ->
-            [ data.Tables.``Skloňování[editovat]``.Rows.[case].singulár ]
+            [ data.Tables.``Skloňování[editovat]``.Rows.[caseColumn].singulár ]
         | Plural -> 
-            [ data.Tables.``Skloňování[editovat]``.Rows.[case].plurál ]
+            [ data.Tables.``Skloňování[editovat]``.Rows.[caseColumn].plurál ]
     | 2 ->
         let data = EditableArticleTwoDeclensions.Parse text
         match number with
         | Singular ->
-            [ data.Tables.``Skloňování (1)[editovat]``.Rows.[case].singulár 
-              data.Tables.``Skloňování (2)[editovat]``.Rows.[case].singulár ]
+            [ data.Tables.``Skloňování (1)[editovat]``.Rows.[caseColumn].singulár 
+              data.Tables.``Skloňování (2)[editovat]``.Rows.[caseColumn].singulár ]
         | Plural -> 
-            [ data.Tables.``Skloňování (1)[editovat]``.Rows.[case].plurál 
-              data.Tables.``Skloňování (2)[editovat]``.Rows.[case].plurál ]
+            [ data.Tables.``Skloňování (1)[editovat]``.Rows.[caseColumn].plurál 
+              data.Tables.``Skloňování (2)[editovat]``.Rows.[caseColumn].plurál ]
     | _ ->
         invalidOp ("Odd word: " + word)
 
-let getLocked case number (NounArticle { Text = text }) =
+let getLocked (case: Case) number (NounArticle { Text = text }) =
     let data = LockedArticle.Parse text
+    let caseColumn = caseColumns.[case]
     match number with
     | Singular ->
-        [ data.Tables.Skloňování.Rows.[case].singulár ]
+        [ data.Tables.Skloňování.Rows.[caseColumn].singulár ]
     | Plural -> 
-        [ data.Tables.Skloňování.Rows.[case].plurál ]
+        [ data.Tables.Skloňování.Rows.[caseColumn].plurál ]
 
 let getDeclinability (NounArticle article) =
     let hasIndeclinabilityMarkInNounSection = 
@@ -80,9 +91,9 @@ let getDeclensionWiki (case: Case) number nounArticle =
     | _ when getDeclinability nounArticle = Indeclinable ->
         [ word ]
     | _ when article |> isEditable ->
-        getEditable (int case) number nounArticle
+        getEditable case number nounArticle
     | _ when article |> isLocked ->
-        getLocked (int case) number nounArticle
+        getLocked case number nounArticle
     | word -> 
         invalidOp ("Odd word: " + word)
 
