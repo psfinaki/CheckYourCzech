53b3c549690d41fd041b33a599e5d332ebb2b1a1
Peter Semkin <petersemkin@gmail.com>
2020-06-23 10:30:52 +0200

    Minor refactoring

--- src/Core/Nouns/NounPatterns.fs
+++ src/Core/Nouns/NounPatterns.fs
@@ -3,13 +3,23 @@
 open Common.Declension
 open Common.GrammarCategories
 
-let patternsGenderMap =
-    dict [ (Gender.MasculineAnimate, MasculineAnimateNounPatternDetector.getPatterns >> Seq.map DeclensionPattern.MasculineAnimate)
-           (Gender.MasculineInanimate, MasculineInanimateNounPatternDetector.getPatterns >> Seq.map DeclensionPattern.MasculineInanimate)
-           (Gender.Feminine, FeminineNounPatternDetector.getPatterns >> Seq.map DeclensionPattern.Feminine)
-           (Gender.Neuter, NeuterNounPatternDetector.getPatterns >> Seq.map DeclensionPattern.Neuter) ]
-
-let getPatternsByGender declension gender = patternsGenderMap.[gender] declension
+let getPatternsByGender declension = function
+    | Gender.MasculineAnimate -> 
+        declension 
+        |> MasculineAnimateNounPatternDetector.getPatterns 
+        |> Seq.map DeclensionPattern.MasculineAnimate
+    | Gender.MasculineInanimate -> 
+        declension 
+        |> MasculineInanimateNounPatternDetector.getPatterns 
+        |> Seq.map DeclensionPattern.MasculineInanimate
+    | Gender.Feminine -> 
+        declension 
+        |> FeminineNounPatternDetector.getPatterns 
+        |> Seq.map DeclensionPattern.Feminine
+    | Gender.Neuter -> 
+        declension 
+        |> NeuterNounPatternDetector.getPatterns 
+        |> Seq.map DeclensionPattern.Neuter
 
 let getPatterns gender declension declinability = 
     match declinability with
