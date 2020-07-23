c46108ebdafd5e93bbcd831843c2e49269cfdf91
Peter Semkin <psfinaki@users.noreply.github.com>
2020-07-23 15:46:47 +0200

    Added contacts to the web (#698)

--- src/Client/Widgets/Navbar.fs
+++ src/Client/Widgets/Navbar.fs
@@ -3,6 +3,7 @@ module Client.Widgets.Navbar
 open Fable.React
 open Fable.React.Props
 open Fulma
+open Fable.FontAwesome
 
 let private navbarView isHome dispatch =
     let backArrow = 
@@ -11,11 +12,25 @@ let private navbarView isHome dispatch =
     let textLink = 
         Heading.p [ Heading.Is4 ]
             [ str "Check Your Czech" ]
+    let contactText = 
+        Heading.h5 [ Heading.CustomClass "contact-text" ] 
+            [ str "Contact" ]
+    let contactIcon = 
+        Icon.icon [ Icon.CustomClass "contact-icon"; Icon.Size IsMedium; Icon.IsRight ]
+                    [ Fa.i [ Fa.Brand.GithubAlt; Fa.Size Fa.Fa2x ] [] ]
+    let contactLink = 
+        Navbar.Item.a 
+            [ Navbar.Item.Props [ 
+                Href "https://github.com/psfinaki/CheckYourCzech"
+                Target "_blank" ] ]
+            [ contactText; contactIcon ] 
+
     div [ ClassName "navbar-bg" ]
         [ Navbar.navbar [ ]
                 [ Navbar.Brand.div [ ]
                     [ Navbar.Item.a [ Navbar.Item.Props [ Href "#" ] ]
                         [ (if isHome then textLink else backArrow) ] 
+                      contactLink
                     ]
                 ] 
         ]
