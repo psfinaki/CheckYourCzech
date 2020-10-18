9f4378a506ffb2b437823b5b6653a452decf1a68
Peter Semkin <petersemkin@gmail.com>
2020-10-18 19:59:47 +0200

    Expecto -> xUnit

--- tests/Client.UiTests/Tests.fs
+++ tests/Client.UiTests/Tests.fs
@@ -1,43 +1,34 @@
 module Client.UiTests.Tests
 
 open canopy.classic
-open Expecto
+open canopy.types
 open System
-open System.IO
+open Xunit
+
+type Fixture() =
+    do
+        //start Chrome // Use this if you want to see your tests in the browser
+        start ChromeHeadless
+        resize (1280, 960)
 
 let serverUrl = Environment.GetEnvironmentVariable("SERVER_URL")
 
-type TypeInThisAssembly() =
-    class end
-
-let screenshotFolder = FileInfo(System.Reflection.Assembly.GetAssembly(typeof<TypeInThisAssembly>).Location).Directory.FullName
-
-let testCase name f =
-    testCase name (fun x ->
-        try
-            f x
-        with
-        | exn ->
-            screenshot screenshotFolder (name  + "-" + System.DateTime.Now.ToString("MMM-d_HH-mm-ss")) |> ignore
-            raise exn
-    )
-
-let startApp () =
+let openApp() =
     url serverUrl
     waitForElement("#elmish-app")
 
-let tests =
-    testList "client tests" [
-        testCase "sound check - server is online" (fun () ->
-            startApp ()
-        )
+type Tests() = 
+    interface IClassFixture<Fixture>
 
-        testCase "validate word is loaded" (fun () ->
-            startApp ()
-
-            click (first ".link-block")
-
-            let taskText = (element ".task-label").GetAttribute("innerText")
-            Expect.isNotNull taskText "should contain text"
-        )
-    ]
+    [<Fact>]
+    member _.``Soundcheck - server is online``() =
+        openApp()
+        
+    [<Fact>]
+    member _.``Validate word is loaded``() =
+        openApp()
+        click (first ".link-block")
+        
+        let taskText = (element ".task-label").GetAttribute("innerText")
+        
+        Assert.NotNull taskText
