dc8debbced20eb3df6728c157f8d171318d6737e
Peter Semkin <petersemkin@gmail.com>
2020-10-22 12:12:53 +0200

    Minor adjustments in console logging

--- src/Scraper/Logging.fs
+++ src/Scraper/Logging.fs
@@ -5,8 +5,8 @@ type LogMessage =
     | Exception of exn * (string * string)
  
 let consoleLogger = function
-    | Trace m -> ConsoleLogger.log m
-    | Exception (exn, _) -> ConsoleLogger.log exn
+    | Trace m -> ConsoleLogger.logTrace m
+    | Exception (exn, _) -> ConsoleLogger.logError exn
 
 let aiLogger = function
     | Trace m -> ApplicationInsightsLogger.logTrace m
