52831f51bcf60369c96f11b31a694dafca9fb448
Peter Semkin <psfinaki@users.noreply.github.com>
2020-10-27 20:45:38 +0100

    Add UI tests for the Task widget (#751)

---
+++ tests/Client.UiTests/Fixture.fs
@@ -0,0 +1,15 @@
+﻿module Client.UiTests.Fixture
+
+open canopy.classic
+open canopy.types
+open System
+
+type Fixture() =
+    do
+        //start Chrome // Use this if you want to see your tests in the browser
+        start ChromeHeadless
+        resize (1280, 960)
+
+    interface IDisposable with 
+        member _.Dispose() =
+            quit()
---
+++ tests/Client.UiTests/WebElementHelper.fs
@@ -0,0 +1,11 @@
+﻿module Client.UiTests.WebElementHelper
+
+open OpenQA.Selenium
+
+let findByTag tag (e: IWebElement) = e.FindElements(By.TagName tag)
+
+let getAttribute name (e: IWebElement) = e.GetAttribute name
+
+let isEnabled (e: IWebElement) = e.Enabled
+
+let getClass (e: IWebElement) = e |> getAttribute "class"
